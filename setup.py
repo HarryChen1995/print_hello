6d400941244c6ec7dd379e6dfcf9448eab8e7d91
hanlinchen <HChen@invoicecloud.com>
2021-06-14 21:17:59 -0400

    updated

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as file:
     file.close()
 
 setuptools.setup(
-    name = "hello_print_hanlin",
+    name = "print_hello_hanlin",
     version = "0.0.1",
     author = "hanlin chen",
-    description = "A simple foo libary",
+    description = "A custom print libary",
     long_description = description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/HarryChen1995/print_hello.git",
