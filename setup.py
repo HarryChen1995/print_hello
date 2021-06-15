51320ce39ed7cac1924d569030f62e9fdd727b7d
Harry Chen <HarryChen1995@yahoo.com>
2021-06-14 21:06:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
     file.close()
 
 setuptools.setup(
-    name = "foo",
+    name = "hello_print_hanlin",
     version = "0.0.1",
     author = "hanlin chen",
     description = "A simple foo libary",
@@ -13,10 +13,5 @@ setuptools.setup(
     long_description_content_type = "text/markdown",
     url = "https://github.com/HarryChen1995/print_hello.git",
     packages = setuptools.find_packages(),
-    classifiers = [
-        "Programming Language :: python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operationg System :: OS Independent"
-    ],
     python_requires = ">=3.5"
 )
